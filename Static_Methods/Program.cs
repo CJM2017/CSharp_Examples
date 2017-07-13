0c1e788e04b3db2872ba8a8f9462cf0c47e04df5
Computron <Computron@COMPUTRON-PC>
2017-07-13 19:53:34 -0400

    Add project files.

---
+++ Static_Methods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;   // Includes String object
+
+
+namespace Static_Methods
+{
+
+    class Test
+    {
+        // Public properties
+        private String name;
+        private int age;
+
+        // Constructor
+        public Test(String N, int A)
+        {
+            this.name = N;
+            this.age = A;
+        }
+
+        // Public methods
+        public static void print_something()
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+        public String get_name()
+        {
+            return this.name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test.print_something();
+
+            Test myTest = new Test("Connor", 25);
+            Console.WriteLine(myTest.get_name());
+
+            //myTest.print_something(); this will not work!
+        }
+    }
+}
